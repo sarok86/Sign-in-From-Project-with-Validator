e6307aef49a7e450fcad5d7dc4f81030abc3c642
Sarok Dastkhosh <saruk.dastkhosh5186@gmail.com>
2023-09-21 11:58:51 +0330

    create touch handler and submit hamdler and install react-tostify

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
 
-import {validate} from "./validate"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import {validate} from "./validate"; 
+import {notify} from "./toast"
 
 const SignUp = () => {
 
-    const [data , setData] = useState ({
+    const [data , setData] = useState({
         name:"",
         email:"",
         password:"",
@@ -13,11 +17,11 @@ const SignUp = () => {
     });
 
     const [errors , setErrors] = useState({});
+    const [touched , setTouched] = useState({});
 
     useEffect(()=>{
         setErrors(validate(data)) 
-        console.log(errors)
-    },[data])
+    },[data,touched])
 
     const changeHandler = event => {
         if(event.target.name === "isAccepted"){
@@ -27,35 +31,61 @@ const SignUp = () => {
         }
     }
 
+    const focusHandler = event =>{
+        setTouched({ ...touched,[event.target.name]: true})
+    }
+
+    const submitHandler =event =>{
+        event.preventDefault();
+        if(!Object.keys(errors).length){
+            notify ("Your signed in  Successfully","success")
+        } else{
+            notify("Invalid Data" , "error")
+            setTouched({
+                name:true,
+                email:true,
+                password:true,
+                confirmPassword:true,
+                isAccepted:true
+            })
+        }
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={submitHandler}>
                 <h2>SignUp</h2>
                 <div>
                     <label>Name</label>
-                    <input type='text'  name='name' value={data.name} onChange={changeHandler} />
+                    <input type='text'  name='name' value={data.name} onChange={changeHandler} onFocus={focusHandler} />
+                    {errors.name && touched.name && <span>{errors.name}</span>}
                 </div>
                 <div>
                     <label>Email</label>
-                    <input type='text'  name='email' value={data.email} onChange={changeHandler} />
+                    <input type='text'  name='email' value={data.email} onChange={changeHandler} onFocus={focusHandler} />
+                    {errors.email && touched.email && <span>{errors.email}</span>}
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type='password'  name='password' value={data.password} onChange={changeHandler} />
+                    <input type='password'  name='password' value={data.password} onChange={changeHandler} onFocus={focusHandler} />
+                    {errors.password && touched.password && <span>{errors.password}</span>}
                 </div>
                 <div>
                     <label>Confirm Password</label>
-                    <input type='password'  name='confirmPassword' value={data.confirmPassword} onChange={changeHandler} />
+                    <input type='password'  name='confirmPassword' value={data.confirmPassword} onChange={changeHandler} onFocus={focusHandler} />
+                    {errors.confirmPassword && touched.confirmPassword && <span>{errors.confirmPassword}</span>}
                 </div>
                 <div>
                     <label>I accept terms of privacy police</label>
-                    <input type='checkbox'  name='isAccepted' value={data.isAccepted} onChange={changeHandler} />
+                    <input type='checkbox'  name='isAccepted' value={data.isAccepted} onChange={changeHandler} onFocus={focusHandler} />
+                    {errors.isAccepted && touched.isAccepted && <span>{errors.isAccepted}</span>}
                 </div>
                 <div>
                     <a href='#'>Login</a>
                     <button type='submit'>Sing Up</button>
                 </div>
             </form>
+            <ToastContainer />
         </div>
     );
 };
